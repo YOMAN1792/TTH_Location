6382c2cd33b66ef62f0fa3fbd2710b1d5beff1b5
YOMAN1792 <94007829+YOMAN1792@users.noreply.github.com>
2024-07-11 19:02:50 -0400

    Update secret.lua

--- secret.lua
+++ secret.lua
@@ -1,4 +1,4 @@
 Secret = {
     --Add your own discord webhook for logs
-    DiscordWebhook = 'https://discord.com/api/webhooks/1242162338333851818/Yq-ZNmL8bBA6Ze4TAP2_c84TUKGe_r4-shYe5Wt9fpJCeYbn5W3QD5dgi2cjMjqvmABw'
+    DiscordWebhook = "PUT_YOUR_WEBHOOK_HERE"
 }
