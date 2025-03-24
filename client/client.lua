2e259c01154bff4ac057778c3d6ff5606b881fbb
YOMAN1792 <Lysol2@hotmail.ca>
2025-03-23 21:30:57 -0400

    Remove car spawning logic from client.lua to streamline vehicle management

--- client/client.lua
+++ client/client.lua
@@ -86,35 +86,6 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Register a network event that spawns a car when triggered
-RegisterNetEvent('TTH_Location.Location:spawnCar')
-AddEventHandler('TTH_Location.Location:spawnCar', function(car)
-    local playerPed = PlayerPedId()
-    local coords = vector3(Config.VehiclePosition.x, Config.VehiclePosition.y, Config.VehiclePosition.z)
-    local heading = Config.VehiclePosition.heading
-
-    -- Show a notification when the car is spawned
-    ESX.ShowAdvancedNotification(
-        Language[Config.lang].NotifVehicleSpawn["sender"],
-        Language[Config.lang].NotifVehicleSpawn["subject"],
-        Language[Config.lang].NotifVehicleSpawn["msg"],
-        Config.textureDict, 1
-    )
-
-    ESX.Game.SpawnVehicle(car, coords, heading, function(vehicle)
-        if DoesEntityExist(vehicle) then
-            SetEntityAsMissionEntity(vehicle, true, true)
-            SetVehicleNumberPlateText(vehicle, Config.PlateName)
-            TaskWarpPedIntoVehicle(playerPed, vehicle, -1)
-
-            -- Send a Discord log
-            TriggerServerEvent('TTH_Location.Location:DiscordLog', GetPlayerServerId(PlayerId()), GetPlayerName(PlayerId()), car)
-        else
-            debugPrint("^1Error: Vehicle spawn failed -> " .. tostring(car))
-        end
-    end, true)
-end)
-
 -- PED
 Citizen.CreateThread(function()
     local pedModel = "cs_lazlow"
